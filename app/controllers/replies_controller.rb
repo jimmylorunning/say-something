1395152996d8eb7f975420c9c54b6f71b967c7ea
Jimmy <jimmylorunning@gmail.com>
2015-01-04 10:10:50 -0500

    Add inline reply, take out replies/new.html.erb option

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -6,18 +6,18 @@ class RepliesController < ApplicationController
 
 	def create
     raise "Error: user not signed in" if !user_signed_in?
-# do I have to do this or will it be automatic?
-#		@post = Posts.find_by_id(params.require(:reply).permit(:post_id))
-    @reply = Replies.create!(post_params.merge(user: current_user))
+    @reply = Replies.create!(post_params.merge(
+      user: current_user,
+      post_id: params[:post_id]))
     flash[:notice] = "Reply added!"
-    redirect_to post_path(params[:reply][:post_id])
+    redirect_to post_path(params[:post_id])
 	end
 
 
   private
 
     def post_params
-      params.require(:reply).permit(:content, :post_id)
+      params.require(:reply).permit(:content)
     end
 
 end
